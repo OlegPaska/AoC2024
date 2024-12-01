8af23bd5fe0395f47cd09f1de52898b3d2338683
OlegPaska <olegpaska.2005@gmail.com>
2024-12-01 13:05:07 +0000

    day 1

---
+++ day1/AOC1.py
@@ -0,0 +1,23 @@
+f = open("in.txt", 'r')
+l1 = []
+l2 = []
+for line in f:
+  l1.append(int(line.split()[0]))
+  l2.append(int(line.split()[1]))
+#q1
+# l1.sort()
+# l2.sort()
+# diff = 0
+# for i in range(len(l1)):
+#   diff += abs(l1[i]-l2[i])
+# print(diff)
+
+#q2 o(n^2)
+similarity = 0
+for i in l1:
+  occurances = 0
+  for j in l2:
+    if j == i:
+      occurances +=1
+  similarity += occurances * i
+print(similarity)
